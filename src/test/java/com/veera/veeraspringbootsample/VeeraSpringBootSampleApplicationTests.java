95e2ba55dc1dd885fc6849270b8b8a53caedab9e
raghavaboddu <raghavaboddu@gmail.com>
2019-12-27 14:35:25 -0500

    test

---
+++ src/test/java/com/veera/veeraspringbootsample/VeeraSpringBootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veera.veeraspringbootsample;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class VeeraSpringBootSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
